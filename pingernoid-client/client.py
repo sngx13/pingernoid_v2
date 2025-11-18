d29aca835d72368559e4bfc8b810b367f276cbce
sngx13 <serghei.golipad@gmail.com>
2025-11-18 16:50:55 +0000

    .

--- pingernoid-client/client.py
+++ pingernoid-client/client.py
@@ -1,65 +1,47 @@
-import sqlite3
 import logging
-from ipaddress import ip_address
+import sqlite3
+from datetime import datetime
+from uuid import uuid4
 from .database import init_db, DB
-from .models import Target
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def existing_target(ip_addr: ip_address) -> bool:
-    logger.info(f"=> Looking up target IP: {ip_addr}")
-    try:
-        con = sqlite3.connect(DB)
-        cur = con.cursor()
-        fetch_results = cur.execute("""SELECT * FROM targets WHERE ip_addr = ?""", (ip_addr,)).fetchall()
-        if len(fetch_results) > 0:
-            data = fetch_results[0]
-            logger.info(f"=> Found existing target IP: {data[0]} in the database...")
-            return True
-        else:
-            logger.info(f"=> Could not find any ICMP probes to the target IP: {ip_addr}")
-            return False
-    except Exception as e:
-        logger.error(f"=> Error: {e} -> {type(e)}")
-        return False
-    finally:
-        con.close()
-
-
-def add_target(target: Target) -> bool:
-    logger.info("=> Attempting to add a target...")
-    try:
-        con = sqlite3.connect(DB)
-        cur = con.cursor()
-        exists = existing_target(ip_addr=target.ip_addr)
-        if exists:
-            logger.info(f"=> Existing probe is already running towards: {target.ip_addr}...")
-            return False
-        else:
-            logger.info(f"=> Adding new ICMP target: {target.ip_addr}")
-            cur.execute(
-                """INSERT INTO targets VALUES (?, ?, ?, ?, ?, ?)""",
-                (str(target.ip_addr), target.count, target.timeout, target.size, target.wait, target.interval),
-            )
-            con.commit()
-            return True
-    except Exception as e:
-        logger.error(f"=> Error: {e} -> {type(e)}")
-        return False
-    finally:
-        con.close()
+def existing_system_id():
+    con = sqlite3.connect(DB)
+    cur = con.cursor()
+    fetch_results = cur.execute("""SELECT id FROM system""").fetchall()
+    if fetch_results:
+        id = fetch_results[0]
+        return id
 
 
 def main():
-    init_db()
-    create_target = Target(ip_addr="8.8.8.8", count=10, timeout=1, size=200, wait=0.05, interval=180)
-    target = add_target(target=create_target)
-    if target:
-        logger.info("=> Successfully added probe...")
+    try:
+        init_db()
+        con = sqlite3.connect(DB)
+        cur = con.cursor()
+        fetch_results = cur.execute("""SELECT id FROM system""").fetchall()
+        if fetch_results:
+            id = fetch_results[0][0]
+            logger.info(f"=> Found existing system id: {id}")
+        else:
+            id = str(uuid4())
+            logger.info(f"=> System is starting up with: {id}")
+            cur.execute(
+                """INSERT INTO system VALUES (?, ?)""",
+                (
+                    id,
+                    str(datetime.now().replace(microsecond=0)),
+                ),
+            )
+            con.commit()
+    except Exception as e:
+        logger.info(f"=> Error: {e} -> {type(e)}")
+    finally:
+        con.close()
 
 
 if __name__ == "__main__":
-    logger.info("=> Starting...")
     main()
--- pingernoid-client/database.py
+++ pingernoid-client/database.py
@@ -9,6 +9,26 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
+def create_system_table() -> None:
+    logger.info("=> Creating 'system' table...")
+    try:
+        con = sqlite3.connect(DB)
+        cur = con.cursor()
+        cur.execute(
+            """
+                CREATE TABLE IF NOT EXISTS system (
+                    id TEXT NOT NULL,
+                    timestamp TEXT NOT NULL
+                )
+            """
+        )
+        con.commit()
+    except Exception as e:
+        logger.info(f"=> Error: {e} -> {type(e)}")
+    finally:
+        con.close()
+
+
 def create_targets_table() -> None:
     logger.info("=> Creating 'targets' table...")
     try:
@@ -63,5 +83,6 @@ def create_results_table() -> None:
 
 def init_db():
     logger.info("=> Creating tables...")
+    create_system_table()
     create_targets_table()
     create_results_table()

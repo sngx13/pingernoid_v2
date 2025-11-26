8d61d9cac5f2367deb069b5140e86f10f9c59473
sngx13 <serghei.golipad@gmail.com>
2025-11-26 15:43:01 +0000

    .

--- pingernoid-client/main.py
+++ pingernoid-client/main.py
@@ -2,54 +2,11 @@ import re
 import subprocess
 from datetime import datetime
 from fastapi import BackgroundTasks, Depends, FastAPI, HTTPException
-from ipaddress import ip_address
-from pydantic import field_validator
 from sqlmodel import Field, Session, SQLModel, String, create_engine, select
 from typing import Annotated
 from uuid import uuid4, UUID
 
-
-class Result(SQLModel, table=True):
-    __tablename__ = "results"
-    id: UUID | None = Field(default_factory=uuid4, primary_key=True)
-    ip_addr: str = Field(sa_type=String(15), index=True)
-    timestamp: datetime
-    sent: int
-    rcvd: int
-    loss: float
-    rtt_min: float
-    rtt_avg: float
-    rtt_max: float
-
-    @field_validator("ip_addr", mode="before")
-    @classmethod
-    def validate_ip(cls, v):
-        if isinstance(v, str):
-            ip_address(v)
-            return v
-        return str(v)
-
-
-class TargetBase(SQLModel):
-    ip_addr: str = Field(sa_type=String(15))
-    count: int
-    timeout: int
-    size: int
-    wait: float
-    interval: int
-
-    @field_validator("ip_addr", mode="before")
-    @classmethod
-    def validate_ip(cls, v):
-        if isinstance(v, str):
-            ip_address(v)
-            return v
-        return str(v)
-
-
-class Target(TargetBase, table=True):
-    __tablename__ = "targets"
-    id: UUID | None = Field(default_factory=uuid4, primary_key=True)
+from .models import Result, Target, TargetBase
 
 
 sqlite_file_name = "pingernoid.db"
